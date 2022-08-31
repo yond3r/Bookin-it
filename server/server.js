91e155793b08f1f1cf5f4c45818a7247c57bc2d6
Hope Barrow <barr.writing@gmail.com>
2022-08-31 10:39:28 -0500

    changes to server

--- server/server.js
+++ server/server.js
@@ -1,10 +1,25 @@
 const express = require('express');
+const {ApolloServer} = require('apollo-sever-express');
+const {authMiddleware} = require('./utils/auth');
+
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
+const {typeDefs, resolvers} = require('./schemas');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+//apollo server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+})
+
+//integration
+server.applyMiddlewear({app});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,8 +29,21 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+  app.get('*', (req, res)=> {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+// app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+
+  //GQL API
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+
 });
+
+process.on('uncaughtException', function(err){
+  console.log('caught exception: ' + err);
+})
+
