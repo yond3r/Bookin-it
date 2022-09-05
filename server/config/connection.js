486d367ab83ac293bfa57cc2cac2c8f07b8f7b56
Hope Barrow <barr.writing@gmail.com>
2022-09-05 17:53:19 -0500

    more heroku attempts

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 
 //so mongoose 6 says that useCreateIndex and useFindAndMotify are no longer supported? I don't know if there is anything to replace them with. 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+mongoose.connect
+  (process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', 
+  {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   // useCreateIndex: true,
