cfe349666ca0c397a8c3953500b7ce120e583621
Hope Barrow <barr.writing@gmail.com>
2022-09-05 10:51:32 -0500

    fixing utils

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,7 +1,7 @@
 import gql from 'graphql-tag';
 
-export const LOGIN_USER = gql 
-    `mutation login($email: String!, $password: String!){
+export const LOGIN_USER = gql `
+mutation login($email: String!, $password: String!){
         login(email: $email, password: $password){
             token
             user{
@@ -9,8 +9,8 @@ export const LOGIN_USER = gql
                 username
             }}}`;
 
-export const ADD_USER = gql
-    `mutation addUser($username: String!, $email: String!, $password: String!){
+export const ADD_USER = gql`
+mutation addUser($username: String!, $email: String!, $password: String!){
         addUser(username: $username, email: $email, password: $password){
             user{
                 _id
@@ -28,8 +28,8 @@ export const ADD_USER = gql
             token
         }}`;
 
-export const SAVE_BOOK = gql
-    `mutation saveBook($input: savedBook!){
+export const SAVE_BOOK = gql`
+mutation saveBook($input: savedBook!){
         saveBook (input: $input)
         {
             _id
@@ -46,8 +46,8 @@ export const SAVE_BOOK = gql
                 description
             }}}`;
 
-export const REMOVE_BOOK = gql
-`mutation removeBook($bookId: ID!){
+export const REMOVE_BOOK = gql`
+mutation removeBook($bookId: ID!){
     removeBook(bookId: $bookId){
         _id
         username
