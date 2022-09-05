d365f1afee0985ef53a327df1e21995920102e1f
Hope Barrow <barr.writing@gmail.com>
2022-09-05 12:57:09 -0500

    fixing paths again

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -4,7 +4,7 @@ import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap
 import { useQuery, useMutation } from '@apollo/react-hooks';
 import { GET_ME } from '../utils/queries';
 import { REMOVE_BOOK } from '../utils/mutations';
-import { Auth } from '../utils/localStorage';
+import  Auth  from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
 
 const SavedBooks = () => {
