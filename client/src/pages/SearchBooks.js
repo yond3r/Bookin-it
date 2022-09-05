9aea53608b4b0168d83cdd06f3a7e4dbebe66224
Hope Barrow <barr.writing@gmail.com>
2022-09-05 10:58:14 -0500

    fixing path

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -6,7 +6,7 @@ import { searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 import {useMutation} from '@apollo/react-hooks';
 import {SAVE_BOOK} from '..utils/mutations';
-import {GET_ME} from 'utils/queries';
+import {GET_ME} from '..utils/queries';
 
 const SearchBooks = () => {
   // create state for holding returned google api data
