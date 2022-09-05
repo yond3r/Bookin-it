fae93e161cf7693ba1cb4acd03f01c2388f5297c
Hope Barrow <barr.writing@gmail.com>
2022-09-05 13:11:56 -0500

    more minis

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
@@ -29,11 +29,11 @@ function App() {
       <Router>
         <>
         <Navbar />
-        <Switch>
+        <Routes>
           <Route exact path = '/' component={SearchBooks} />
           <Route exact path = '/saved' component={SavedBooks} />
           <Route render= {() => <h1 className ='display-2'>Wrong Page!</h1>} />
-          </Switch>
+          </Routes>
         </>
       </Router>
     </ApolloProvider>
