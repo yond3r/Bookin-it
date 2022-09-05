83fb4b4fd0cb947bdd29c0d86d94157c6ba6493b
Hope Barrow <barr.writing@gmail.com>
2022-09-05 11:13:09 -0500

    fixing paths

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
 
 import {useQuery, useMutation} from '@apollo/react-hooks';
-import {GET_ME} from '..utils/queries';
+import {GET_ME} from '../utils/queries';
 import {REMOVE_BOOK} from '..utils/mutations';
 import Auth from '../utils/localStorage';
 import { removeBookId } from '../utils/localStorage';
